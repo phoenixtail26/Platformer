c3bf00a8772b3f4f3c7b9f5a3f76ac3b0eacae86
Gavin Hayler <ghayler@gmail.com>
2012-07-29 15:56:01 +0200

    - Moved player input code into separate player input class

---
+++ Assets/Code/PlayerInput.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour 
+{
+	public PlayerMovementController player;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		TastyInput i = TastyInput.instance;
+		i.BindAxes( "Move", JoystickAxes.LeftAnalogStick );
+		i.BindAxesKeys( "Move", KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.DownArrow, KeyCode.UpArrow );
+		
+		i.BindKey( "Jump", KeyCode.Z );
+		i.BindButton( "Jump", JoystickButtons.A_Cross );
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		TastyInput i = TastyInput.instance;
+		Vector2 inputVec = Vector2.zero;
+		
+		inputVec = i.GetAxes( "Move" );
+				
+		player.Move( inputVec );
+		
+		if ( i.GetButtonDown( "Jump" ) )
+		{
+			player.StartJump();
+		}
+		
+		if ( i.GetButtonUp( "Jump" ) )
+		{
+			player.EndJump();
+		}
+	}
+}
